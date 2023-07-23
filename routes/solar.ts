bd7989195f042524488c3c1ac075e16d209fdb89
Chris Harrington <chrisharrington99@gmail.com>
2023-07-23 15:19:49 +0000

    Add endpoint for retrieving solar wattage and generation data, and another for inserting generation stats.

--- routes/solar.ts
+++ routes/solar.ts
@@ -12,16 +12,33 @@ const mongo = new MongoClient(Secret.mongoConnectionString);
 export default ((app: Application) => {
     app.get('/solar', getSolarData);
     app.post('/solar', insertSolarData);
+    app.get('/solar/generation', getSolarGeneration);
+    app.post('/solar/daily', insertDailySolarGeneration);
 });
 
-const getSolarData = (request: Request, response: Response) => {
+const getSolarData = async (request: Request, response: Response) => {
     try {
-        const start = request.query.start as string,
-            end = request.query.end as string;
+        console.log('GET /solar');
+        console.log(JSON.stringify(request.query, null, 4));
 
-        console.log(dayjs(start).format(), dayjs(end).format());
+        if (!request.query.start)
+            return response.status(400).send('Missing start date.');
+        if (!request.query.end)
+            return response.status(400).send('Missing end date.');
 
-        response.sendStatus(200);
+        const start = dayjs(request.query.start as string),
+            end = dayjs(request.query.end as string),
+            db = mongo.db('home'),
+            collection = db.collection('watts');
+
+        const results = await collection.find({
+            timestamp: {
+                $gte: start.toDate(),
+                $lte: end.toDate()
+            }
+        }).toArray();
+
+        response.status(200).send(JSON.stringify(results));
     } catch (e) {
         console.error(e);
         response.status(500).send(e);
@@ -34,11 +51,60 @@ const insertSolarData = async (request: Request, response: Response) => {
         console.log(JSON.stringify(request.body, null, 4));
 
         const db = mongo.db('home'),
-            collection = db.collection('solar');
+            collection = db.collection('watts');
 
         await collection.insertOne({
-            timestamp: dayjs.utc().toISOString(),
+            timestamp: dayjs.utc().toDate(),
             currentEnergy: request.body.energy
+        });
+
+        response.sendStatus(200);
+    } catch (e) {
+        console.error(e);
+        response.status(500).send(e);
+    }
+}
+
+const getSolarGeneration = async (request: Request, response: Response) => {
+    try {
+        console.log('GET /solar/generation');
+        console.log(JSON.stringify(request.query, null, 4));
+
+        if (!request.query.start)
+            return response.status(400).send('Missing start date.');
+        if (!request.query.end)
+            return response.status(400).send('Missing end date.');
+
+        const start = dayjs(request.query.start as string),
+            end = dayjs(request.query.end as string),
+            db = mongo.db('home'),
+            collection = db.collection('generation');
+
+        const results = await collection.find({
+            timestamp: {
+                $gte: start.toDate(),
+                $lte: end.toDate()
+            }
+        }).toArray();
+
+        response.status(200).send(JSON.stringify(results));
+    } catch (e) {
+        console.error(e);
+        response.status(500).send(e);
+    }
+}
+
+const insertDailySolarGeneration = async (request: Request, response: Response) => {
+    try {
+        console.log('POST /solar/generation');
+        console.log(JSON.stringify(request.body, null, 4));
+
+        const db = mongo.db('home'),
+            collection = db.collection('generation');
+
+        await collection.insertOne({
+            timestamp: dayjs.utc().toDate(),
+            daily: request.body.generation
         });
 
         response.sendStatus(200);
