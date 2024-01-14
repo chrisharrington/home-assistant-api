fd92eeb160ff79fa7a0ff6ead36bf6b3de94afae
Chris Harrington <chrisharrington99@gmail.com>
2024-01-14 17:12:32 +0000

    Update energy logging routes to send a message to the Telegram bot.

--- routes/energy.ts
+++ routes/energy.ts
@@ -2,70 +2,17 @@ import { Application, Request, Response } from 'express';
 import dayjs from 'dayjs';
 import { MongoClient } from 'mongodb';
 import utc from 'dayjs/plugin/utc';
+import { sendTelegramMessage } from '@root/notifications/bot';
 dayjs.extend(utc);
 
 const mongo = new MongoClient(process.env.MONGO_CONNECTION_STRING);
 
 export default ((app: Application) => {
-    app.get('/energy', getEnergyData);
-    app.post('/energy', insertEnergyData);
     app.get('/energy/generation', getEnergyGeneration);
     app.post('/energy/generation/daily', insertDailyEnergyGeneration);
     app.get('/energy/usage', getEnergyUsage);
     app.post('/energy/usage/daily', insertDailyEnergyUsage);
 });
-
-const getEnergyData = async (request: Request, response: Response) => {
-    try {
-        console.log('GET /energy');
-        console.log(JSON.stringify(request.query, null, 4));
-
-        if (!request.query.start)
-            return response.status(400).send('Missing start date.');
-        if (!request.query.end)
-            return response.status(400).send('Missing end date.');
-
-        const start = dayjs(request.query.start as string),
-            end = dayjs(request.query.end as string),
-            db = mongo.db('home'),
-            collection = db.collection('watts');
-
-        const results = await collection.find({
-            timestamp: {
-                $gte: start.toDate(),
-                $lte: end.toDate()
-            }
-        }).toArray();
-
-        response
-            .status(200)
-            .set('Content-Type', 'application/json')
-            .send(JSON.stringify(results.map(r => ({ timestamp: r.timestamp, value: r.currentEnergy }))));
-    } catch (e) {
-        console.error(e);
-        response.status(500).send(e);
-    }
-}
-
-const insertEnergyData = async (request: Request, response: Response) => {
-    try {
-        console.log('POST /energy:');
-        console.log(JSON.stringify(request.body, null, 4));
-
-        const db = mongo.db('home'),
-            collection = db.collection('watts');
-
-        await collection.insertOne({
-            timestamp: dayjs.utc().toDate(),
-            currentEnergy: request.body.energy
-        });
-
-        response.sendStatus(200);
-    } catch (e) {
-        console.error(e);
-        response.status(500).send(e);
-    }
-}
 
 const getEnergyGeneration = async (request: Request, response: Response) => {
     try {
@@ -115,10 +62,13 @@ const insertDailyEnergyGeneration = async (request: Request, response: Response)
             daily: request.body.generation
         });
 
+        sendTelegramMessage(`Yesterday's energy generation was ${request.body.generation} kWh.`);
+
         response.sendStatus(200);
     } catch (e) {
         console.error(e);
         response.status(500).send(e);
+        sendTelegramMessage('An error occurred while updating daily energy generation.');
     }
 }
 
@@ -160,7 +110,6 @@ const getEnergyUsage = async (request: Request, response: Response) => {
 const insertDailyEnergyUsage = async (request: Request, response: Response) => {
     try {
         console.log('POST /energy/usage');
-        console.log(JSON.stringify(request.body, null, 4));
 
         const db = mongo.db('home'),
             collection = db.collection('usage');
@@ -170,9 +119,12 @@ const insertDailyEnergyUsage = async (request: Request, response: Response) => {
             daily: request.body.usage
         });
 
+        sendTelegramMessage(`Yesterday's energy usage was ${request.body.usage} watts.`);
+
         response.sendStatus(200);
     } catch (e) {
         console.error(e);
         response.status(500).send(e);
+        sendTelegramMessage('An error occurred while updating daily energy usage.');
     }
 }
